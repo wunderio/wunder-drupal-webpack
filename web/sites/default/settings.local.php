651ed0193fffb59b16fda9e994917bc2948302d6
Dāniels Brūvers <daniels.bruvers@wunder.io>
2020-11-25 17:32:38 +0200

    Add config files to enable Twig debug mode

---
+++ web/sites/default/settings.local.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @file
+ * Settings for local development.
+ */
+
+/**
+ * Enable local development services.
+ */
+$settings['container_yamls'][] = DRUPAL_ROOT . '/sites/local.services.yml';
