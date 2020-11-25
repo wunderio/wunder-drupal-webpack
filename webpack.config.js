9a53574e233aa3162f2c656b926c23e461bf7086
Dāniels Brūvers <daniels.bruvers@wunder.io>
2020-11-25 15:35:09 +0200

    Use underscore instead of dash in theme name

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const appThemeConfig = require('./web/themes/custom/wunder-theme/webpack.config');
+const appThemeConfig = require('./web/themes/custom/wunder_theme/webpack.config');
 // Optionally add more webpack config files and...
 // const moduleConfig = require('./drupal/web/modules/custom/wunder-module/webpack.config');
 // ...add them to the array of exports like this
